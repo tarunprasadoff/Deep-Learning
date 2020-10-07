306b48d1da6e62f4ab2b5e08e6094a63faf90397
tarunprasadoff <56791139+tarunprasadoff@users.noreply.github.com>
2020-10-07 20:20:33 +0530

    Filter Resolve

--- Assignment_1/1/Stride/2.py
+++ Assignment_1/1/Stride/2.py
@@ -77,18 +77,17 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=2, stride=2)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=2, stride=2)
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=2, stride=2)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=4, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2)
         self.fc1 = nn.Linear(in_features=256, out_features=64)
         self.fc2 = nn.Linear(in_features=64, out_features=32)
         self.fc3 = nn.Linear(in_features=32, out_features=5)      # change out_features according to number of classes
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
         x = F.avg_pool2d(x, kernel_size=x.shape[2:])
         x = x.view(x.shape[0], -1)
         x = F.relu(self.fc1(x))
